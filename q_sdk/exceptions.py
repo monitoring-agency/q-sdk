b85a72cf8155a55413943920c36dab0ba0925e58
myOmikron <accounts@omikron.dev>
2021-08-23 02:07:39 +0200

    Added exception for http statuscodes

--- q_sdk/exceptions.py
+++ q_sdk/exceptions.py
@@ -1,1 +1,6 @@
 
+class HttpStatusCodeException(Exception):
+    def __init__(self, status_code, msg):
+        self.status_code = status_code
+        self.msg = msg
+        super(HttpStatusCodeException, self).__init__(msg)
