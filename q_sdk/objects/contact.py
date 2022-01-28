7262633414454a67e3904df47bffb40b7062bc20
myOmikron <accounts@omikron.dev>
2022-01-29 00:06:25 +0100

    Corrected models and added new attributes

--- q_sdk/objects/contact.py
+++ q_sdk/objects/contact.py
@@ -4,9 +4,11 @@ from objects.base import Base
 
 
 class Contact(Base):
-    def __init__(self, name, mail="", linked_host_notifications=None, linked_host_notification_period=None,
-                 linked_metric_notifications=None, linked_metric_notification_period=None, variables=None):
+    def __init__(self, name=None, mail=None, linked_host_notifications=None, linked_host_notification_period=None,
+                 linked_metric_notifications=None, linked_metric_notification_period=None, variables=None, id=None,
+                 comment=None):
         super(Contact, self).__init__()
+        self.id = id
         self.name = name
         self.mail = mail
         self.linked_host_notifications = linked_host_notifications
@@ -14,6 +16,7 @@ class Contact(Base):
         self.linked_metric_notifications = linked_metric_notifications
         self.linked_metric_notification_period = linked_metric_notification_period
         self.variables = variables
+        self.comment = comment
 
 
 class ContactParam(enum.Enum):
--- q_sdk/objects/global_variable.py
+++ q_sdk/objects/global_variable.py
@@ -4,10 +4,12 @@ from objects.base import Base
 
 
 class GlobalVariable(Base):
-    def __init__(self, key, value):
+    def __init__(self, key=None, value=None, id=None, comment=None):
         super(GlobalVariable, self).__init__()
+        self.id = id
         self.key = key
         self.value = value
+        self.comment = comment
 
 
 class GlobalVariableParam(enum.Enum):
